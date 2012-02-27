940d64bf0603ff3200908b489d229ad6f5c4d35a
sekky <halseki@gmail.com>
2012-02-27 23:36:37 +0900

    change listen port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // socket.ioイベント登録
